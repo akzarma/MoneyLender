8d30ec23533845c7daf504f5d2adbfab91d6e8de
akzarma <akzarma@gmail.com>
2018-06-28 00:06:01 +0530

    customer and account class

--- app/src/main/java/com/oxvsys/moneylender/Customer.java
+++ app/src/main/java/com/oxvsys/moneylender/Customer.java
@@ -1,8 +1,58 @@
 package com.oxvsys.moneylender;
 
 public class Customer {
-    String name;
-    String occupation;
+    private String id;
+    private String name;
+    private String occupation;
+    private String aadhar_id;
+    private String mobile;
+    private String DOB;
 
+    public String getId() {
+        return id;
+    }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    public String getAadhar_id() {
+        return aadhar_id;
+    }
+
+    public void setAadhar_id(String aadhar_id) {
+        this.aadhar_id = aadhar_id;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
 }
