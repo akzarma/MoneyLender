343eb95c6f8de8ae25b54385b3c7de4f81c08aa8
akzarma <akzarma@gmail.com>
2018-06-30 14:12:55 +0530

    commit new

--- app/src/main/java/com/oxvsys/moneylender/FragmentDailyLoan.java
+++ app/src/main/java/com/oxvsys/moneylender/FragmentDailyLoan.java
@@ -19,10 +19,12 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
@@ -56,8 +58,9 @@ public class FragmentDailyLoan extends Fragment {
 
     Agent agent_selected;
     List<Agent> agentList = new ArrayList<>();
-//    String cust_id = "8";
+    //    String cust_id = "8";
     Spinner spinner;
+    Long lastAccountNo;
 
     private OnFragmentInteractionListener mListener;
 
@@ -93,13 +96,13 @@ public class FragmentDailyLoan extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_fragment_daily_loan, container, false);
 
-        Button save_button =  view.findViewById(R.id.grant_button_monthly);
+        Button save_button = view.findViewById(R.id.grant_button_monthly);
         final EditText account_no = view.findViewById(R.id.account_number_monthly_grant);
-        final EditText edit_amount =  view.findViewById(R.id.amount_monthly_grant);
-        final EditText edit_o_date =  view.findViewById(R.id.start_date_monthly_grant);
-        final EditText edit_c_date =  view.findViewById(R.id.end_date_monthly_grant);
-        final EditText edit_roi =  view.findViewById(R.id.roi_monthly_grant);
-
+        final EditText edit_amount = view.findViewById(R.id.amount_monthly_grant);
+        final EditText edit_o_date = view.findViewById(R.id.start_date_monthly_grant);
+        final EditText edit_c_date = view.findViewById(R.id.end_date_monthly_grant);
+//        final EditText edit_roi = view.findViewById(R.id.roi_monthly_grant);
+        final TextView prefix_account_no = view.findViewById(R.id.account_number_prefix_field);
         spinner = view.findViewById(R.id.agent_spinner);
 
         final Customer selected_customer = (Customer) getArguments().getSerializable(ARG_PARAM1);
@@ -156,6 +159,20 @@ public class FragmentDailyLoan extends Fragment {
             }
         });
 
+        DatabaseReference myref = database.getReference("lastAccountNo");
+        myref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                prefix_account_no.setText((dataSnapshot.getValue(Long.class) + 1) + " - ");
+                lastAccountNo = dataSnapshot.getValue(Long.class);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
 
         DatabaseReference agent_db_ref = database.getReference("agents");
 
@@ -167,7 +184,7 @@ public class FragmentDailyLoan extends Fragment {
                     agent1.setId(agent.getKey());
                     agentList.add(agent1);
                 }
-                Collections.sort(agentList,new Comparator<Agent>() {
+                Collections.sort(agentList, new Comparator<Agent>() {
                     @Override
                     public int compare(Agent o1, Agent o2) {
                         return o1.getId().compareTo(o2.getId());
@@ -175,7 +192,7 @@ public class FragmentDailyLoan extends Fragment {
                 });
                 List<String> agents = new ArrayList<>();
                 for (Agent each : agentList) {
-                    agents.add(each.getId() + " - "+ each.getName().split(" ")[0]);
+                    agents.add(each.getId() + " - " + each.getName().split(" ")[0]);
                 }
                 ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_dropdown_item, agents);
                 spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -208,32 +225,41 @@ public class FragmentDailyLoan extends Fragment {
 
             @Override
             public void onClick(View v) {
+                final String final_account_no;
+                if (account_no.getText().toString().equals("")) {
+                    final_account_no = String.valueOf(lastAccountNo + 1);
+                } else {
+                    final_account_no = (lastAccountNo + 1) + "-" + account_no.getText().toString();
+                }
 
                 DatabaseReference agent_customer = database.getReference("agentAccount");
-                agent_customer.child(agent_selected.getId()).child(account_no.getText().toString()).setValue("");
+                agent_customer.child(agent_selected.getId()).child(final_account_no).setValue(selected_customer.getId());
 
                 String o_date = edit_o_date.getText().toString();
                 String c_date = edit_c_date.getText().toString();
                 String amount = edit_amount.getText().toString();
-                String roi = edit_roi.getText().toString();
+//                String roi = edit_roi.getText().toString();
 
                 DatabaseReference customers = database.getReference("customers").child(selected_customer.getId()).child("accounts");
 
                 Map<String, String> account_number_details = new HashMap<>();
-                account_number_details.put("no", account_no.getText().toString());
+
+                account_number_details.put("no", final_account_no);
                 account_number_details.put("amt", amount);
                 account_number_details.put("o_date", o_date);
                 account_number_details.put("c_date", c_date);
-                account_number_details.put("roi", roi);
+//                account_number_details.put("roi", roi);
                 account_number_details.put("type", "monthly");
 
                 Map<String, Object> map = new HashMap<>();
-                map.put(account_no.getText().toString(), account_number_details);
+                map.put(final_account_no, account_number_details);
 
                 customers.updateChildren(map, new DatabaseReference.CompletionListener() {
                     @Override
                     public void onComplete(@Nullable DatabaseError databaseError, @NonNull DatabaseReference databaseReference) {
                         Log.d("monthly stuff", "onComplete: " + databaseError);
+                        DatabaseReference lastAccountNo_ref = database.getReference("lastAccountNo");
+                        lastAccountNo_ref.setValue(lastAccountNo + 1);
                     }
                 });
             }
@@ -276,4 +302,6 @@ public class FragmentDailyLoan extends Fragment {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
+
 }
